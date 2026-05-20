74b22b0d645ef7bc16552cdae9600390f4c2edd1
tep <2644231680@qq.com>
2026-05-20 23:57:25 +0800

    feat: add route fallback migration

---
+++ db/migrations/000002_route_fallback.up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE route_policies
+  DROP CONSTRAINT route_policies_strategy_check;
+
+ALTER TABLE route_policies
+  ADD CONSTRAINT route_policies_strategy_check
+  CHECK (strategy IN ('single', 'fallback'));
